112d2e9bdd9358e552d27760dbd7e680eb80ad93
AJ Jones <alexander.t.jones92@gmail.com>
2021-03-27 16:15:43 -0700

    Added "attending" functionality, working for multiple attendance.

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,6 +1,15 @@
 const {buildSchema} = require('graphql');
 
 module.exports = buildSchema(`
+
+type Attending{
+    _id: ID!
+    event: Event!
+    user: User!
+    createdAt: String!
+    updatedAt: String!
+}
+
 type Event{
     _id: ID!
     title: String!
@@ -31,11 +40,15 @@ input EventInput{
 
 type RootQuery{
     events: [Event!]!
+    attendings: [Attending!]!
 }
 type RootMutation{
     createEvent(eventInput: EventInput): Event
     createUser(userInput: UserInput): User
+    attendEvent(eventId: ID): Attending!
+    cancelAttending(attendingId: ID!): Event!
 }
+
 schema {
     query: RootQuery
     mutation: RootMutation
