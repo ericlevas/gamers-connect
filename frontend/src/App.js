1c0fa0388b75f8f5137f848d0ddaa61deece487c
AJ Jones <alexander.t.jones92@gmail.com>
2021-03-28 13:30:56 -0700

    Basic user creation and login setup. Navigation between pages and auto redirects added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,21 +5,45 @@ import AuthPage from './pages/Auth';
 import EventPage from './pages/Events';
 import AttendingPage from './pages/Attending';
 import MainNavigation from './components/navigation/MainNavigation';
+import AuthContext from './context/auth-context';
 
 class App extends Component {
+
+  state = {
+    token: null,
+    userId: null
+  }
+
+  login = (token, userId, tokenExpiration) => {
+    this.setState({ token: token, userId: userId });
+  };
+
+  logout = () => {
+    this.setState({token: null, userId: null});
+  };
+
   render() {
     return (
       <BrowserRouter>
         <React.Fragment>
-          <MainNavigation />
-          <main className="main-content">
-            <Switch>
-              <Redirect from="/" to="/auth" exact />
-              <Route path="/auth" component={AuthPage} />
-              <Route path="/events" component={EventPage} />
-              <Route path="/attending" component={AttendingPage} />
-            </Switch>
-          </main>
+          <AuthContext.Provider value={{
+            token: this.state.token,
+            userId: this.state.userId,
+            login: this.login,
+            logout: this.logout
+          }}>
+            <MainNavigation />
+            <main className="main-content">
+              <Switch>
+                {!this.state.token && <Redirect from="/" to="/auth" exact />}
+                {this.state.token && <Redirect from="/" to="/events" exact />}
+                {this.state.token && <Redirect from="/auth" to="/events" exact />}
+                {!this.state.token && <Route path="/auth" component={AuthPage} />}
+                <Route path="/events" component={EventPage} />
+                {this.state.token && <Route path="/attending" component={AttendingPage} />}
+              </Switch>
+            </main>
+          </AuthContext.Provider>
         </React.Fragment>
       </BrowserRouter>
     );
