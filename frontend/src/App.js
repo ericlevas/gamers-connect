6bb9314428fa94117461431057d88d7f94749bd8
AJ Jones <alexander.t.jones92@gmail.com>
2021-03-27 19:51:54 -0700

    basic styling for navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
 import './App.css';
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import EventPage from './pages/Events';
 import AttendingPage from './pages/Attending';
+import MainNavigation from './components/navigation/MainNavigation';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/events" component={EventPage} />
-          <Route path="/attending" component={AttendingPage} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventPage} />
+              <Route path="/attending" component={AttendingPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
