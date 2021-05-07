4409f4538dacee3eeb83e6b9b13caa5c7523f4b4
Eric Levas <levas003@cougars.csusm.edu>
2021-05-07 13:32:07 -0700

    add working calendar event creation

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -7,6 +7,11 @@ import Spinner from '../components/spinner/Spinner';
 import { Redirect } from 'react-router-dom';
 import Logo from '../../media/logo.png'
 import Calendar from './Calendar';
+import FullCalendar, { formatDate } from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import { INITIAL_EVENTS, createEventId } from '../event-utils'
 
 export default class EventPage extends Component {
     state = {
@@ -17,6 +22,10 @@ export default class EventPage extends Component {
         selectedEvent: null,
         weekendsVisible: true,
         currentEvents: [],
+        weekendsVisible: true,
+        currentEvents: [],
+        startD: null,
+        endD: null,
     };
     isActive = true;
 
@@ -90,12 +99,12 @@ export default class EventPage extends Component {
         this.setState({ creating: false });
         const title = this.titleEl.current.value;
         const gameTitle = this.gameTitleEl.current.value;
-        const startDate = this.startDateEl.current.value;
-        const endDate = this.endDateEl.current.value;
+        const startDate = this.state.startD;
+        const endDate = this.state.endD;
         const description = this.descriptionEl.current.value;
         //const event = { title, gameTitle, date, description };
 
-        if (title.trim().length === 0 || gameTitle.trim().length === 0 || this.startDateEl.current.value.trim().length === 0 || this.startDateEl.current.value.trim().length === 0 || description.trim().length === 0) {
+        if (title.trim().length === 0 || gameTitle.trim().length === 0 || description.trim().length === 0) {
             return;
         }
 
@@ -171,7 +180,7 @@ export default class EventPage extends Component {
     };
 
     modalCancelHandler = () => {
-        this.setState({ creating: false, selectedEvent: null });
+        this.setState({ creating: false, selectedEvent: null, viewing: false });
     };
 
     showDetailHandler = eventId => {
@@ -224,7 +233,6 @@ export default class EventPage extends Component {
             .catch(err => {
                 console.log(err);
             });
-
     }
 
     render() {
@@ -257,41 +265,6 @@ export default class EventPage extends Component {
                             </AuthContext.Consumer>
                         </React.Fragment >
                         <React.Fragment>
-                            {(this.state.creating || this.state.selectedEvent) && <Backdrop />}
-                            {this.state.creating && (
-                                <Modal
-                                    title="New Event"
-                                    canCancel
-                                    canConfirm
-                                    onCancel={this.modalCancelHandler}
-                                    onConfirm={this.modalConfirmHandler}
-                                    confirmText="Confirm"
-                                >
-                                    <form>
-                                        <div className="form-control">
-                                            <label htmlFor="title">Title</label>
-                                            <input type="text" id="title" ref={this.titleEl}></input>
-                                        </div>
-                                        <div className="form-control">
-                                            <label htmlFor="gameTitle">Game Title</label>
-                                            <input type="text" id="gameTitle" ref={this.gameTitleEl}></input>
-                                        </div>
-                                        <div className="form-control">
-                                            <label htmlFor="startDate">Date</label>
-                                            <input type="datetime-local" id="startDate" ref={this.startDateEl}></input>
-                                        </div>
-                                        <div className="form-control">
-                                            <label htmlFor="endDate">Date</label>
-                                            <input type="datetime-local" id="endDate" ref={this.endDateEl}></input>
-                                        </div>
-                                        <div className="form-control">
-                                            <label htmlFor="description">description</label>
-                                            <textarea id="description" rows="4" ref={this.descriptionEl}></textarea>
-                                        </div>
-                                    </form>
-                                </Modal>
-                            )
-                            }
                             {this.state.selectedEvent &&
                                 (<Modal
                                     title={this.state.selectedEvent.title}
@@ -307,15 +280,15 @@ export default class EventPage extends Component {
                                 </Modal>)
                             }
 
-                            {this.context.token && (<div className="events-control">
+                            {/*{this.context.token && (<div className="events-control">
                                 <p>Share your own Events!</p>
                                 <button className="submit-button" onClick={this.startCreateEventHandler}>
                                     Create Event
                                 </button>
                             </div>
                             )
-                            }
-                            <h2>My Events</h2>
+                            }*/}
+                            <br /><h2>All Events</h2>
                             {this.state.isLoading ?
                                 (<Spinner />)
                                 :
@@ -329,7 +302,39 @@ export default class EventPage extends Component {
                     </div>
                 </div>
                 <div className='app-main'>
-                    <Calendar />
+                    <div className='header'>
+                        <label>
+                            <input
+                                type='checkbox'
+                                checked={this.state.weekendsVisible}
+                                onChange={this.handleWeekendsToggle}
+                            ></input>
+                        toggle weekends
+                        </label>
+                    </div>
+                    <FullCalendar
+                        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                        headerToolbar={{
+                            left: 'prev,next today',
+                            center: 'title',
+                            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                        }}
+                        initialView='dayGridMonth'
+                        editable={true}
+                        selectable={true}
+                        selectMirror={true}
+                        dayMaxEvents={true}
+                        weekends={this.state.weekendsVisible}
+                        initialEvents={INITIAL_EVENTS}
+                        //events={INITIAL_EVENTS}
+                        select={this.handleDateSelect}
+                        eventContent={renderEventContent}
+                        eventClick={this.handleEventClick}
+                        eventsSet={this.handleEvents}
+                    //eventAdd={function(){}}
+                    //eventChange={function(){}}
+                    //eventRemove={function(){}}
+                    />
                     {(this.state.creating) && <Backdrop />}
                     {this.state.creating && (
                         <Modal
@@ -348,14 +353,6 @@ export default class EventPage extends Component {
                                 <div className="form-control">
                                     <label htmlFor="gameTitle">Game Title</label>
                                     <input type="text" id="gameTitle" ref={this.gameTitleEl}></input>
-                                </div>
-                                <div className="form-control">
-                                    <label htmlFor="startDate">Date</label>
-                                    <input type="datetime-local" id="startDate" ref={this.startDateEl}></input>
-                                </div>
-                                <div className="form-control">
-                                    <label htmlFor="endDate">Date</label>
-                                    <input type="datetime-local" id="endDate" ref={this.endDateEl}></input>
                                 </div>
                                 <div className="form-control">
                                     <label htmlFor="description">description</label>
@@ -382,4 +379,86 @@ export default class EventPage extends Component {
             </div>
         );
     }
+
+    handleWeekendsToggle = () => {
+        this.setState({
+            weekendsVisible: !this.state.weekendsVisible
+        })
+    }
+    
+    handleDateSelect = (selectInfo) => {
+        if (this.context.token) {
+            this.setState({ creating: true });
+            this.setState({ startD: selectInfo.startStr });
+            this.setState({ endD: selectInfo.endStr });
+        }
+        else {
+            alert('Please login to create an event.')
+        }
+        //let title = prompt('Please enter a new title for your event')
+
+        let calendarApi = selectInfo.view.calendar;
+        calendarApi.unselect() // clear date selection
+
+        // if (title) {
+        //   calendarApi.addEvent({
+        //     id: createEventId(),
+        //     title,
+        //     start: selectInfo.startStr,
+        //     end: selectInfo.endStr,
+        //     allDay: selectInfo.allDay
+        //   })
+        // }
+    }
+
+    handleEventClick = (clickInfo) => {
+        if (this.context.token) {
+            this.setState({ viewing: true });
+        }
+        else {
+            alert('Please login to view an event.')
+        }
+
+        //Conditional: If !token
+
+        //If user is event owner, include delete button
+        //Conditional: If token && (userID == creatorID)
+        //if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+        //clickInfo.event.remove()
+        //Update events database and attending database
+
+        //If user is not event owner or member of group, include join button
+        //Conditional: If token && (userID != creatorID) && !attending
+        //Update attending database
+
+        //If user is not event owner but is member of group, include leave button
+        //Conditional: If token && (userID != creatorID) && attending
+        //Update attending database
+
+        //}
+    }
+
+    handleEvents = (events) => {
+        this.setState({
+            currentEvents: events
+        })
+    }
+}
+
+function renderEventContent(eventInfo) {
+    return (
+        <>
+            <b>{eventInfo.timeText}</b>
+            <i>{eventInfo.event.title}</i>
+        </>
+    )
+}
+
+function renderSidebarEvent(event, token) {
+    return (
+        <li key={event.id}>
+            <b>{formatDate(event.start, { year: 'numeric', month: 'short', day: 'numeric' })}</b>
+            <i>{event.title}</i>
+        </li>
+    )
 }
