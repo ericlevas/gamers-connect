3ff11d0055c1485ec5758727d48351aff4f99892
AJ Jones <alexander.t.jones92@gmail.com>
2021-05-10 16:34:34 -0700

    Added third sidebar category for groups that you have created.

--- client/src/components/events/eventlist/EventList.js
+++ client/src/components/events/eventlist/EventList.js
@@ -4,17 +4,21 @@ import EventItem from '../eventItem/EventItem.js'
 const eventList = props => {
     const events = props.events.map(event => {
         return (
-            <EventItem
-                key={event._id}
-                eventId={event._id}
-                title={event.title}
-                gameTitle={event.gameTitle}
-                start={event.start}
-                end={event.end}
-                userId={props.authUserId}
-                creatorId={event.creator._id}
-                onDetail={props.onViewDetail}
-            />);
+            props.authUserId !== event.creator._id ? 
+                (<EventItem
+                    key={event._id}
+                    eventId={event._id}
+                    title={event.title}
+                    gameTitle={event.gameTitle}
+                    start={event.start}
+                    end={event.end}
+                    userId={props.authUserId}
+                    creatorId={event.creator._id}
+                    onDetail={props.onViewDetail}
+                />) 
+                : 
+                null 
+            );
     });
 
     return <ul className="events__list">{events}</ul>;
